0f8605b6f8d38000f054027cae00e4afbb8659ae
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-07-06 00:58:04 +0600

    fix: comment out MongoDB ping command and log in index.js; add vercel.json for deployment configuration

--- index.js
+++ index.js
@@ -159,8 +159,8 @@ async function run() {
       res.send(result);
     });
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
   }
 }
